3c78151475da274e9f6cb6d59953327baf95c618
gtdev <gtdevrandom@gmail.com>
2026-01-21 15:07:12 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Configuration Firebase de ton application
 const firebaseConfig = {
   apiKey: "AIzaSyCJ-N_nexROudL48v6voYzwLjp00DELJRk",
   authDomain: "fightzoneio.firebaseapp.com",
@@ -17,12 +10,10 @@ const firebaseConfig = {
   measurementId: "G-15RQQKCKDX"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-// Initialisation de Firebase
+// Initialisation de Firebase (Syntaxe Version 8)
 firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
 
+// On rend les services disponibles pour game.js
+const database = firebase.database();
 const auth = firebase.auth();
+const analytics = firebase.analytics();
